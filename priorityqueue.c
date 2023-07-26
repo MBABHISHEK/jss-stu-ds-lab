f7b0a948e9ce76762de472006a44f43fe2f8ad8c
ABHISHEK M B <117739148+MBABHISHEK@users.noreply.github.com>
2023-07-26 13:26:08 +0530

    Create priorityqueue.c

---
+++ priorityqueue.c
@@ -0,0 +1,67 @@
+#include<stdio.h>
+#include<stdlib.h>
+int size;
+struct queue
+{
+int *item;
+int f,r,cnt;
+};
+typedef struct queue QUE;
+void insert(QUE *q)
+{
+if(q->cnt==size)
+{
+printf("Queue Full\nReallocating\n");
+q->item=((int *)realloc(q->item,4*size*sizeof(int)));
+size=size*4;
+}
+printf("Enter Item : ");int item;scanf("%d",&item);
+int j=q->r;
+while(j>=0 && *(q->item+q->r)>item)
+{
+*(q->item+j+1)=*(q->item+j);
+j--;
+}
+*(q->item+j+1)=item;
+q->r++;
+q->cnt++;
+}
+void delete(QUE *q)
+{
+if(q->cnt==0)
+{
+printf("EMPTY\n");
+}
+printf("delted %d\n",*(q->item+q->f));
+q->f=((q->f+1)%size);
+q->cnt--;
+}
+void display(QUE q)
+{
+if(q.cnt==0)
+return;
+while(q.f<=q.r)
+{
+printf("%d ",*(q.item+q.f));
+q.f++;
+}
+printf("\n");
+
+}
+void main()
+{
+printf("Enter Size of Queue : ");scanf("%d",&size);
+QUE q;
+q.item=((int *)malloc(sizeof(int)*size));
+q.cnt=0;q.f=0;q.r=-1;
+for(;;)
+{
+printf("1.Insert\n2.delete\nEnter : ");int ch;scanf("%d",&ch);
+switch(ch)
+{
+case 1:insert(&q);display(q);break;
+case 2:delete(&q);display(q);break;
+default:exit(0);
+}
+}
+} 
